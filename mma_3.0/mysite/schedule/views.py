79cd0088f5279f47483813838f27f8ae14d04e0f
Nick Pettigrew <nickpettigrew@Nicks-MacBook-Pro.local>
2018-04-24 09:02:37 +1000

    Template file created and configured. Home Page now created based on template

--- mma_3.0/mysite/schedule/views.py
+++ mma_3.0/mysite/schedule/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'schedule/index.html')
+
+# Landing Page View, from generic_template.html
+class HomePageView(TemplateView):
+	def get(self, request, **kwargs):
+		return render(request, 'index.html', context=None)
