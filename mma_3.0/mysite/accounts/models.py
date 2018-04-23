2f83e65a08835b70223e79b82be0a74fe226d331
jacobbullock95 <jacobbullock95@gmail.com>
2018-04-23 18:38:02 +1000

    ..

--- mma_3.0/mysite/accounts/models.py
+++ mma_3.0/mysite/accounts/models.py
@@ -5,5 +5,20 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
 	#Add extra fields here
-	#test_field = models.CharField(max_length = 2, default = "Hi")
-	pass
+
+	#To Add:
+
+	#phone
+	#street number
+	#street name
+	#suburb
+	#state
+	#postcode
+
+	#under18?
+	#guardian firstName
+	#guardian lastName
+
+
+	test_field = models.CharField(max_length = 2, default = "Hi")
+	
