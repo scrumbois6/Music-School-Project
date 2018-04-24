50ee4c1a17fb397d4e307a311c1239b2b4ab0e7d
Nick Pettigrew <nickpettigrew@Nicks-MacBook-Pro.local>
2018-04-24 13:16:49 +1000

    Nick Changes

--- mma_3.0/mysite/mysite/urls.py
+++ mma_3.0/mysite/mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('schedule/', include('schedule.urls')),
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/schedule/')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
