7e14fbe64f92e71fecafbc80267673e280d4350f
Alex <a.jbutler019@gmail.com>
2018-05-02 13:43:41 +1000

    Cleaning up git repository

--- mma_3.0/mysite/schedule/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('schedule', views.schedule, name='schedule'),
-    url(r'^$', views.HomePageView.as_view())
-]
