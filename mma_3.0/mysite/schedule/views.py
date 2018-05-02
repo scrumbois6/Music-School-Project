3cf246b80dcb595a543ef02bd06ed43d6760630b
jacobbullock95 <jacobbullock95@gmail.com>
2018-05-02 18:44:07 +1000

    Display lesson info for only the logged in users lessons

--- mma_3.0/mysite/schedule/views.py
+++ mma_3.0/mysite/schedule/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from .models import Lesson
+from django.contrib.auth.models import User
 
 #defining index page view
-def index(request):
-    return render(request, 'schedule/index.html')
+def index(request):   
+	lessons = Lesson.objects.all()
+	return render(request, 'schedule/index.html', context={'lessons':lessons})
 
 def instruments(request):
 	return render(request, 'schedule/instruments.html')
@@ -16,3 +19,4 @@ def profile(request):
 class HomePageView(TemplateView):
 	def get(self, request, **kwargs):
 		return render(request, 'index.html', context=None)
+
